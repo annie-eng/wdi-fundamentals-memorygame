041c5927f6638b6b5f6827307d21aeb5ad2db909
Luane Souza <luanesouza@172-2-1-245.lightspeed.dybhfl.sbcglobal.net>
2019-01-03 20:33:42 -0500

    created game logic

--- memory-game/main.js
+++ memory-game/main.js
@@ -1,4 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardOne;
+var cardsInPlay = ["cardOne", "cardTwo", "cardThree", "cardFour"];
+
+if(cardsInPlay.length === 2){
+console.log("There are " + cardsInPlay.length + "cards");
+}
+    if (cards[0] === cards[1]) {
+        console.log("You found a match");
+    } else {
+        console.log("Try again!");
+    }
+
+
+console.log("User flipped ");
