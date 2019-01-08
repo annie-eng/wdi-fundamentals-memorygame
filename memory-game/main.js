e3db38686a5ccebf1582742f27352fa3be6985a9
Luane Souza <luanesouza@Luanes-MBP.home>
2019-01-08 16:22:42 -0500

    created logic game

--- memory-game/main.js
+++ memory-game/main.js
@@ -1,15 +1,58 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardOne;
-var cardsInPlay = ["cardOne", "cardTwo", "cardThree", "cardFour"];
+var cards = [
+    {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "../images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "../images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "../images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "../images/king-of-diamonds.png"
+},
+];
+    var cardsInPlay = [];
 
-if(cardsInPlay.length === 2){
-console.log("There are " + cardsInPlay.length + "cards");
-}
-    if (cards[0] === cards[1]) {
-        console.log("You found a match");
-    } else {
-        console.log("Try again!");
-    }
+    function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+        } else {
+        alert("Sorry, try again.");
+        };
+};
 
+    function flipCard(){
+        var cardId = this.getAttribute('data-id');
+        cardsInPlay.push(cards[cardId].rank);
+        this.setAttribute('src', cards [cardId].rank);
+        // console.log("User flipped " + cards[cardId].rank);
+        // console.log("The card flipped was "+ cards.cardImage);
+        // console.log("The kind is " +cards.suit);
+        if(cardsInPlay.length === 2){
+         checkForMatch();
+        }
+        
+    };
 
-console.log("User flipped ");
+    function createBoard() {
+        const cardTable = document.getElementById('game-board');
+        for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+          cardElement.setAttribute('src', '../images/back.png');
+          cardElement.setAttribute('data-id', i);
+          cardElement.addEventListener('click', flipCard);
+        //   cardTable.appendChild(cardElement);
+      
+        }
+      };
+
+    createBoard();
